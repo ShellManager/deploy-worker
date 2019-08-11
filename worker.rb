0d1645ca97efc8843ca560edba024c46e41fa10d
Theo Morra <td512@td512.io>
2019-08-11 23:50:20 +1200

    Adds basic polling database worker

---
+++ worker.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+require 'pg'
+require 'yaml'
+
+secrets = YAML.load_file('secrets.yml')
+config = YAML.load_file('config.yml')
+rows = Array.new
+
+def run_every(seconds)
+    last_tick = Time.now
+    loop do
+      sleep 0.1
+      if Time.now - last_tick >= seconds
+        last_tick += seconds
+        yield
+      end
+    end
+end
+
+begin
+
+    conn = PG.connect :dbname => config['database'], :user => secrets['username'], 
+        :password => secrets['password']
+    
+    puts "Successfully logged on to PostgreSQL database #{conn.db} as PostgreSQL user #{conn.user}!"
+    puts "Looking for new rows with state #{config["state"]} every #{config["seek_interval"]} seconds"
+    run_every(5) do
+        res = conn.exec("SELECT * FROM \"#{config["table"]}\" WHERE status = 'pending'")
+        res.each do |row|
+            if ! rows.include? row
+                rows.push(row)
+                puts "Found new pending VM!"
+                puts "Name: #{row["name"]}"
+            end
+        end
+    end
+
+rescue PG::Error => e
+
+    puts e.message 
+    
+ensure
+
+    conn.close if conn
+    
+end
