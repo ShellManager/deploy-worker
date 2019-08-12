73f752ff6f166ec0b7700a476c24eae9b6c94e53
Keanu73 <guestone63@gmail.com>
2019-08-12 10:28:12 +0100

    Move UUID generation to erb file

--- worker.rb
+++ worker.rb
@@ -24,7 +24,6 @@ def create_vm(name, cpus, memory, volume_capacity, ip)
     # stolen from here https://github.com/andrewgho/genmac
     mac = (1..3).collect { "%02x" % [rand 255] }.join(":")
     mac = "AE:AE:AE:#{mac}"
-    uuid = SecureRandom.uuid
     vm = ERB.new(File.read("./vm.erb")).result(binding)
     file = file.open("./vm.conf", "w")
     file.puts vm
